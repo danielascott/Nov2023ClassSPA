c8dc857c5f8f9d4b2051d5a2feed0a7658c3f530
Daniel Scott <danielascott@gmail.com>
2023-12-14 19:56:44 -0600

    finished router and navigo

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import { Header, Nav, Main, Footer } from "./components";
 import * as store from "./store";
+import Navigo from "navigo";
+import { capitalize } from "lodash";
+
+const router = new Navigo("/");
 
 function render(state = store.Home) {
   document.querySelector("#root").innerHTML = `
@@ -8,9 +12,24 @@ function render(state = store.Home) {
   ${Main(state)}
   ${Footer()}
   `;
+
+  router.updatePageLinks();
 }
 
-render();
+router
+.on({
+  "/": () => render(),
+  ":view": (params) => {
+    let view = capitalize(params.data.view);
+    if (view in store) {
+      render(store[view]);
+    } else {
+      render(store.Viewnotfound);
+      console.log(`View ${view} not defined`);
+    }
+  },
+})
+.resolve();
 
 // add menu toggle to bars icon in nav bar
 // document.querySelector(".fa-bars").addEventListener("click", () => {
