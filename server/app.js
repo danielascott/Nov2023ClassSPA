5325d2928aa3e0b36b0bd47c0c8b39637416486a
Daniel Scott <danielascott@gmail.com>
2024-01-02 19:04:52 -0600

    Refactor mongoose connection and pizza creation routes

--- server/app.js
+++ server/app.js
@@ -8,16 +8,13 @@ dotenv.config();
 
 const app = express();
 
-mongoose.connect(
-  process.env.MONGODB,
-  {
-      // Configuration options to remove deprecation warnings, just include them to remove clutter
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-  }
-)
+mongoose.connect(process.env.MONGODB, {
+  // Configuration options to remove deprecation warnings, just include them to remove clutter
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-const db = mongoose.connection
+const db = mongoose.connection;
 
 db.on("error", console.error.bind(console, "Connection Error:"));
 db.once(
@@ -94,35 +91,12 @@ app.get("/weather/:city", (request, response) => {
     rainy,
     temp: {
       current: temp,
-      low: lowTemp
+      low: lowTemp,
     },
-    city
+    city,
   });
 });
 
 app.use("/pizzas", pizzas);
 
 app.listen(PORT, () => console.log("Listening on port 4040"));
-
-// BASIC HTTP SERVER BELOW
-
-// // 'Import' the http module
-// import http from "http";
-// // Initialize the http server
-// const server = http
-//   .createServer((request, response) => {
-//     // Handle the request from http://localhost:4040/status with HTTP GET method
-//     if (request.url === "/status" && request.method === "GET") {
-//       // Create the headers for response
-//       response.writeHead(200, { "Content-Type": "application/json" });
-//       // Create the response body
-//       response.write(JSON.stringify({ message: "Service healthy" }));
-//       // End and return the response
-//       response.end();
-//     }
-//   })
-//   // Tell the HTTP server to start listening
-//   .listen(4040);
-
-// // Let the humans know I am running and listening on 4040
-// console.log("Listening on port 4040");
