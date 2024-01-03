7533388a187638debb6d11ac1adaa4977bd6ccba
Daniel Scott <danielascott@gmail.com>
2024-01-02 21:32:41 -0600

    Update Viewnotfound.js and pizzas.js

--- server/routers/pizzas.js
+++ server/routers/pizzas.js
@@ -22,4 +22,49 @@ router.post("/", async (request, response) => {
   }
 });
 
+// Get all pizzas route
+router.get("/", async (request, response) => {
+  try {
+    // Store the query params into a JavaScript Object
+    const query = request.query; // Defaults to an empty object {}
+
+    const data = await Pizza.find(query);
+
+    response.json(data);
+  } catch (error) {
+    // Output error to the console incase it fails to send in response
+    console.log(error);
+
+    return response.status(500).json(error.errors);
+  }
+});
+
+// Get a single pizza by ID
+router.get("/:id", async (request, response) => {
+  try {
+    const data = await Pizza.findById(request.params.id);
+
+    response.json(data);
+  } catch (error) {
+    // Output error to the console incase it fails to send in response
+    console.log(error);
+
+    return response.status(500).json(error.errors);
+  }
+});
+
+// Delete a pizza by ID
+router.delete("/:id", async (request, response) => {
+  try {
+    const data = await Pizza.findByIdAndRemove(request.params.id, {});
+
+    response.json(data);
+  } catch (error) {
+    // Output error to the console incase it fails to send in response
+    console.log(error);
+
+    return response.status(500).json(error.errors);
+  }
+});
+
 export default router;
